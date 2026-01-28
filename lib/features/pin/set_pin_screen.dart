89360e2dabb8de9454a4b62563cfbc27cd6511c3
Benjamin Obetta <bobetta@parkway.ng>
2026-01-29 00:17:04 +0100

    set transaction pin screen ui

---
+++ lib/features/pin/set_pin_screen.dart
@@ -0,0 +1,111 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import '../../core/utils/app_colors.dart';
+import '../../core/widgets/password_input_field.dart';
+import '../../core/widgets/primary_button.dart';
+
+class SetPinScreen extends ConsumerStatefulWidget {
+  const SetPinScreen({super.key});
+
+  @override
+  ConsumerState<SetPinScreen> createState() => _SetPinScreenState();
+}
+
+class _SetPinScreenState extends ConsumerState<SetPinScreen> {
+  final _formKey = GlobalKey<FormState>();
+  final _newPinController = TextEditingController();
+  final _confirmPinController = TextEditingController();
+  bool _loading = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Set Pin'),),
+      body: _buildBody(),
+    );
+  }
+
+  Widget _buildBody() {
+    return SafeArea(
+      child: Column(
+        children: [
+          const Divider(),
+          _buildForm(),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildForm() {
+    return Expanded(
+      child: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                Center(
+                  child: Text(
+                    'Reset PIN',
+                    style: TextStyle(
+                      fontWeight: FontWeight.w500,
+                      fontSize: 24.0.sp,
+                      color: AppColors.textNeutral,
+                    ),
+                  ),
+                ),
+                SizedBox(height: 8.0.h),
+                Center(
+                  child: Text(
+                    'Reset your default PIN',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontWeight: FontWeight.w400,
+                      color: AppColors.textNeutral2,
+                      fontSize: 14.sp,
+                    ),
+                  ),
+                ),
+                SizedBox(height: 24.0.h),
+                PasswordInputField(
+                  controller: _newPinController,
+                  hint: "New PIN",
+                  label: "New PIN",
+
+                  validator: (String? value) {
+                    if (value!.length != 4) {
+                      return "PIN must be 4 digits";
+                    }
+                    return null;
+                  }, // Ensure this validator exists
+                ),
+                SizedBox(height: 24.0.h),
+                PasswordInputField(
+                  controller: _confirmPinController,
+                  isConfirmPassword: true,
+                  hint: 'Confirm New PIN',
+                  label: 'Confirm New PIN',
+                  validator: (String? value) {
+                    if (_newPinController.text != _confirmPinController.text) {
+                      return "PIN do not match";
+                    }
+                    return null;
+                  },
+                ),
+                SizedBox(height: 50.0.h),
+                PrimaryButton(
+                  isLoading: _loading,
+                  onPressed: (){},
+                  buttonText: 'Reset',
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
