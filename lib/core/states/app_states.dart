0e4198b5b8a0e8d3b97c9a29b0914a27db1a02db
Benjamin Obetta <obettabenjamin44@gmail.com>
2026-02-05 15:00:59 +0100

    podfile update

--- lib/core/states/app_states.dart
+++ lib/core/states/app_states.dart
@@ -4,7 +4,6 @@
 import 'package:belema_test_app/core/models/account_details_model.dart';
 import 'package:belema_test_app/core/models/transaction_model.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutter_riverpod/legacy.dart';
 
 final walletBalance = StateProvider<double>((ref) => 0.0);
 final transactions = StateProvider<List<Transaction>>((ref) => []);
